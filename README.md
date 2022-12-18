ce2ad14fd27c05f2df5a7b7946bb19d61cc11a34
= <otterlord.dev@gmail.com>
2022-12-18 22:02:21 +0000

    I'll commit to improving this later

---
+++ README.md
@@ -0,0 +1,3 @@
+# Branch
+
+A quick attempt to build a links page for myself. I may refurbish this later.
---
+++ src/config.ts
@@ -0,0 +1,48 @@
+interface Link {
+  title: string
+  body: string
+  url?: string
+}
+
+export const NAME = "Otterlord"
+
+export const LINKS: Link[] = [
+  {
+    title: "Website",
+    body: "Visit my website",
+    url: "https://otterlord.dev"
+  },
+  {
+    title: "GitHub",
+    body: "Browse my GitHub profile",
+    url: "https://github.com/theotterlord"
+  },
+  {
+    title: "Twitter",
+    body: "Follow me on Twitter before it collapses",
+    url: "https://twitter.com/theotterlord"
+  },
+  {
+    title: "Medium",
+    body: "Read my technical articles",
+    url: "https://medium.com/@otterlord"
+  },
+  {
+    title: "Blog",
+    body: "Coming soon™",
+  },
+  {
+    title: "Stack Overflow",
+    body: "Flexing miscellaneous knowledge",
+    url: "https://stackoverflow.com/users/14104186/the-otterlord"
+  },
+  {
+    title: "YouTube",
+    body: "Watch my dumb shorts",
+    url: "https://www.youtube.com/@otterlord"
+  },
+  {
+    title: "Discord",
+    body: "Also coming soon™",
+  }
+]
